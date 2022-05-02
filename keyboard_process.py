0f8686e9de8c0a17020b85ae25cf920049a7b96d
apalleiko <67287383+apalleiko@users.noreply.github.com>
2022-05-02 13:22:06 -0400

    format stuff again

--- keyboard_process.py
+++ keyboard_process.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import rospy
 import numpy as np
 import keyboard
@@ -9,117 +8,111 @@ import sys
 import termios
 import os
 import tty
-from sensor_msgs.msg import Image, CameraInfo
-from geometry_msgs.msg import Point, Pose, Twist, Vector3, Quaternion
 from std_msgs.msg import ColorRGBA, Float32, Bool, Int32
 from user_input.msg import Velocity, JoyCmd
-from cv_bridge import CvBridge, CvBridgeError
-import message_filters
 import math
+
 
 class keyboard_obj(object):
 
-	def __init__(self):
-		rospy.init_node('keyboard', anonymous=True)
-		self.joyconnPub= rospy.Publisher('/joy/connected', Bool, queue_size = 1)
-		self.joycmdPub = rospy.Publisher('/joy/cmd', JoyCmd, queue_size = 10)
-		self.dockPub = rospy.Publisher('/test/docking', Bool, queue_size = 1) # Our custom topic called /test/docking with message type Bool (boolean)
-		# this message is set to true when a character is typed in the command line (see detectCmds_dock). Ex. you may want to set a message to true when you finish a task like docking or pick and place
+    def __init__(self):
+        rospy.init_node('keyboard', anonymous=True)
+        self.joyconnPub = rospy.Publisher('/joy/connected', Bool, queue_size=1)
+        self.joycmdPub = rospy.Publisher('/joy/cmd', JoyCmd, queue_size=10)
+        self.dockPub = rospy.Publisher('/test/docking', Bool,
+                                       queue_size=1)  # Our custom topic called /test/docking with message type Bool (boolean)
+        # this message is set to true when a character is typed in the command line (see detectCmds_dock). Ex. you may want to set a message to true when you finish a task like docking or pick and place
 
-		r = rospy.Rate(60)
-		self.cmd_msg = JoyCmd()
-		self.conn_msg = Bool()
-		self.dock_msg = Bool()
-		self.conn_msg.data = True
-		self.dock_msg.data = False
-		self.mult = 1.0/3.0
-		while not rospy.is_shutdown():
-			self.cmd_msg = self.detectCmds()
-			self.dock_msg = self.detectCmds_dock()
-			# Publish at a frequency of 60 Hz
-			self.joyconnPub.publish(self.conn_msg)
-			self.joycmdPub.publish(self.cmd_msg)
-			self.dockPub.publish(self.dock_msg)
+        r = rospy.Rate(60)
+        self.cmd_msg = JoyCmd()
+        self.conn_msg = Bool()
+        self.dock_msg = Bool()
+        self.conn_msg.data = True
+        self.dock_msg.data = False
+        self.mult = 1.0 / 3.0
+        while not rospy.is_shutdown():
+            self.cmd_msg = self.detectCmds()
+            self.dock_msg = self.detectCmds_dock()
+            # Publish at a frequency of 60 Hz
+            self.joyconnPub.publish(self.conn_msg)
+            self.joycmdPub.publish(self.cmd_msg)
+            self.dockPub.publish(self.dock_msg)
 
-			r.sleep()
+            r.sleep()
 
-	def getch(self):
-		fd = sys.stdin.fileno()
-		old_settings = termios.tcgetattr(fd)
-		try:
-			tty.setraw(sys.stdin.fileno())
-			ch = sys.stdin.readline(1)
-	 
-		finally:
-			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-		return ch
+    def getch(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.readline(1)
 
-	def detectCmds_dock(self):
-		char = self.getch()
-		print(char)
-		msg = Bool()
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
-		if 'p' in char:
-			msg.data = True
-		elif 'o' in char:
-			msg.data = False
-		return msg
+    def detectCmds_dock(self):
+        char = self.getch()
+        print(char)
+        msg = Bool()
 
-	def detectCmds(self):
-		char = self.getch()
-		print(char)
-		msg = JoyCmd()
-		#Detect forward and backward keys assigned to 'w' and 's'
-		if 'w' in char:
-			msg.axis1 += 1.0
-		elif 's' in char:
-			msg.axis1 -= 1.0
+        if 'p' in char:
+            msg.data = True
+        elif 'o' in char:
+            msg.data = False
+        return msg
 
-		#Detect left and right keys 'a' and 'd'
-		elif 'a' in char:
-			msg.axis2 += 1.0
-		elif 'd' in char:
-			msg.axis2 -= 1.0
+    def detectCmds(self):
+        char = self.getch()
+        print(char)
+        msg = JoyCmd()
+        # Detect forward and backward keys assigned to 'w' and 's'
+        if 'w' in char:
+            msg.axis1 += 1.0
+        elif 's' in char:
+            msg.axis1 -= 1.0
 
-		#Detect rotation keys 'q' for counterclockwise, 'e' for clockwise
+        # Detect left and right keys 'a' and 'd'
+        elif 'a' in char:
+            msg.axis2 += 1.0
+        elif 'd' in char:
+            msg.axis2 -= 1.0
 
-		elif 'q' in char:
-			msg.axis3 += 1.0
-		elif 'e' in char:
-			msg.axis3 -= 1.0
+        # Detect rotation keys 'q' for counterclockwise, 'e' for clockwise
 
-
-		elif 'n' in char:
-			msg.btn1 += 1
-		elif 'm' in char:
-			msg.btn1 -= 1
+        elif 'q' in char:
+            msg.axis3 += 1.0
+        elif 'e' in char:
+            msg.axis3 -= 1.0
 
 
-		#Detect keys for multipliers
-		elif 'h' in char:
-			self.mult = 1.0/1000.0
-		elif 'j' in char:
-			self.mult = 1.0/500.0
-		elif 'k' in char:
-			self.mult = 1.0/100.0
+        elif 'n' in char:
+            msg.btn1 += 1
+        elif 'm' in char:
+            msg.btn1 -= 1
 
 
-		#Dummy character that also works to stop the vehicle
-		elif '0' in char:
-			pass
+        # Detect keys for multipliers
+        elif 'h' in char:
+            self.mult = 1.0 / 1000.0
+        elif 'j' in char:
+            self.mult = 1.0 / 500.0
+        elif 'k' in char:
+            self.mult = 1.0 / 100.0
 
 
-		msg.axis1 = msg.axis1*self.mult
-		msg.axis2 = msg.axis2*self.mult
-		msg.axis3 = msg.axis3*self.mult
-		return msg
+        # Dummy character that also works to stop the vehicle
+        elif '0' in char:
+            pass
+
+        msg.axis1 = msg.axis1 * self.mult
+        msg.axis2 = msg.axis2 * self.mult
+        msg.axis3 = msg.axis3 * self.mult
+        return msg
 
 
-
-if __name__=='__main__':
-	node = keyboard_obj()
-	
-
+if __name__ == '__main__':
+    node = keyboard_obj()
 
 # var joy_connected_topic = new ROSLIB.Topic({
 #     ros: ros,
